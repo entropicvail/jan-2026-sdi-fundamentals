b5accc8343f54814a5a9e7a82537b2e76ae4fa7b
MWegenke <matthew.wegenke@galvanize.com>
2026-02-04 17:02:19 -0600

    added basic fetch

--- index.js
+++ index.js
@@ -19,3 +19,20 @@ document.addEventListener('load', getLS())
 let setButton = document.querySelector('#setBtn')
 
 setButton.addEventListener('click', setLS)
+
+let catchSumPokemans = () => {
+  // Step Uno fetch any pokeperson
+  // Step B alter your fetch to fetch the first 9 pokemens
+  // Step 2 follow your heart... your pokeheart
+
+  let pokeman = "placeHolder"
+
+  fetch('https://pokeapi.co/api/v2/pokemon/gengar')
+    .then(res => res.json())
+    .then(data => pokeman = data.moves[8].move.name)
+
+  return pokeman
+
+}
+
+catchSumPokemans()
