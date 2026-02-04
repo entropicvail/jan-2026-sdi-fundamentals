848c08185b38b831e121b13a028d4fff64d05efa
MWegenke <matthew.wegenke@galvanize.com>
2026-02-04 10:03:02 -0600

    loacal storage

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-var element = document.querySelectorAll(".div");
+let counter = 0
+
+function setLS(){
+  localStorage.setItem('brian', counter)
+  counter++
+  getLS('brian')
+}
+
+function getLS(lsKey){
+  console.log(lsKey)
+
+  let lsCount = localStorage.getItem(lsKey)
+  let countDisplay = document.querySelector('#count')
+  countDisplay.innerText = lsCount
+}
+
+document.addEventListener('load', getLS())
+
+let setButton = document.querySelector('#setBtn')
+
+setButton.addEventListener('click', setLS)
